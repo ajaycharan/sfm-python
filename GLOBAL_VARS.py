d4b75a65616fead4ef14727234f3f7a21f18997d
Shreyas Skandan <shreyasskandan@Shreyass-MacBook-Pro-2.local>
2016-10-05 12:03:49 -0400

    created feature match preprocessing script and edited global vars

--- GLOBAL_VARS.py
+++ GLOBAL_VARS.py
@@ -8,5 +8,19 @@
 # ____________________________________________________________________________
 
 MAIN_SFM_DIR = '/Users/shreyasskandan/Desktop/'
+
+# Main folder to store all data
 DATA_DIR = MAIN_SFM_DIR + 'data/'
+# Main code repository
 CODE_DIR = MAIN_SFM_DIR + 'code/'
+# Text file containing camera parameters
+CALIBRATION_FILE = DATA_DIR + 'calibration.txt'
+# Folder containing matched features across image frames
+FEATURES_DIR = DATA_DIR + 'correspondences/'
+# Folder containing image frames (2D)
+INPUT_IMAGE_DIR = DATA_DIR + 'imgs/'
+# Folder to store all visualisation output
+OUTPUT_IMAGE_DIR = DATA_DIR + 'output/'
+# Folder to store any additional documentation
+ADDITIONAL_DOCUMENTATION_DIR = DATA_DIR + 'docs/'
+
