b8ef4788c611c7126fa3f4041976cb314a78bc9d
Shreyas Skandan <shreyasskandan@Shreyass-MacBook-Pro-2.local>
2016-10-05 11:53:17 -0400

    data added. data taken from CIS580 at University of Pennsylvania

---
+++ GLOBAL_VARS.py
@@ -0,0 +1,12 @@
+# ____________________________________________________________________________
+#
+# 						ALL GLOBALLY DEFINED VARIABLES
+#						______________________________
+#
+#			All code written by Shreyas Skandan. Data taken from CIS580
+# 		course at the University of Pennsylvania, taught by Dr. Jianbo Shi.
+# ____________________________________________________________________________
+
+MAIN_SFM_DIR = '/Users/shreyasskandan/Desktop/'
+DATA_DIR = MAIN_SFM_DIR + 'data/'
+CODE_DIR = MAIN_SFM_DIR + 'code/'
