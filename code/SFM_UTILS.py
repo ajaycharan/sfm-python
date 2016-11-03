05092a9dd1fe286800ff36db128db35b562f30a9
ShreyasSkandan <shreyas.ibanez@gmail.com>
2016-11-03 16:02:13 -0400

    working on 8 point ransac for F estimation

--- code/SFM_UTILS.py
+++ code/SFM_UTILS.py
@@ -6,6 +6,7 @@ import pdb
 import re
 import cv2
 import matplotlib.pyplot as plt
+import random
 from GLOBAL_VARS import *
 
 def normalise_homogeneous_pts(points_x):
@@ -40,17 +41,39 @@ def get_inliers_RANSAC(points_x1,points_x2,ransac_threshold=0.00005):
 	Fundamental Matrix estimated from the set of final inliers.
 	'''
 	ransac_MAXITER = 1000
+	estimationMaxCount = 100
 	p = 0.99
 	bestM = []
 	trial_ctr = 0
 	best_inlier_ctr = 0
 	N = 1
-	pdb.set_trace()
+	#pdb.set_trace()
 	x1 = np.append(np.transpose(points_x1),np.ones(points_x1.shape[0]).reshape(1,points_x1.shape[0]),0)
 	x2 = np.append(np.transpose(points_x2),np.ones(points_x2.shape[0]).reshape(1,points_x2.shape[0]),0)
 
 	(x1pts,T1) = normalise_homogeneous_pts(x1)
 	(x2pts,T2) = normalise_homogeneous_pts(x2)
+	x = np.append(x1pts,x2pts,0)
+	num_pts = x.shape[1]
+
+	while N > trial_ctr:
+		emptyCondition = 1
+		ctr = 1
+		while emptyCondition:
+			rand_indices = random.sample(range(1, x.shape[1]), MIN_RANSAC_POINTS)
+			emptyCondition = 0
+			if emptyCondition == 0:
+				F_mat_temp = EstimateFundamentalMatrix(x[:,rand_indices])
+				if not F_mat_temp:
+					emptyCondition = 1
+			ctr += 1
+			if ctr > estimationMaxCount:
+				fprintf("Ransac failed at estimating Fundamental Matrix\n")
+				break
+		(bestMask,F_mat_temp) = evaluateDistance(F_mat_temp,x,ransac_threshold)
+		num_inliers = len(bestMask)
+
+
 
 	pdb.set_trace()
 	return (10,10)
