d5b1c5ff23e65315273fd605eeeda489e355f990
ShreyasSkandan <shreyas.ibanez@gmail.com>
2016-11-02 22:54:49 -0400

    ransac desc

--- code/SFM_UTILS.py
+++ code/SFM_UTILS.py
@@ -7,3 +7,21 @@ import re
 import cv2
 import matplotlib.pyplot as plt
 from GLOBAL_VARS import *
+
+
+def get_inliers_RANSAC(points_x1,points_x2,ransac_threshold=0.00005):
+	'''
+	Function: 
+	To find the best set of point correspondences from a set of 
+	possible correspondences using RAndom Sampling And Consensus.
+
+	Input Arguments:
+	This method takes two point sets [X,Y] from two images along
+	with a threshold to constrain the matching.
+
+	Output:
+	This method returns a mask of indices indicating which of the
+	correspondences are inliers. This method also returns the best
+	Fundamental Matrix estimated from the set of final inliers.
+	'''
+	
